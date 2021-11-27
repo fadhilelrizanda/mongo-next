bb3904211ec01972468fcf1b814dcbca4d21a23a
fadhilelrizanda <fadhilelrizanda@gmail.com>
2021-11-28 02:38:37 +0700

    first commit

--- pages/motor.js
+++ pages/motor.js
@@ -5,7 +5,7 @@ import React from "react";
 
 export default function Motor() {
   const [post, setPost] = React.useState(null);
-
+  var moment = require("moment");
   React.useEffect(() => {
     axios
       .get(`https://testexpressjsiot.herokuapp.com/subscribers`)
@@ -38,78 +38,84 @@ export default function Motor() {
       <main className={styles.main}>
         {post.map((data) => {
           return (
-            <table key={data.id} className="table mt-5">
-              <thead key={data.id}>
-                <tr key={data.id}>
-                  <th key={data.id} scope="col">
-                    Sensor1
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor2
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor3
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor4
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor5
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor6
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor7
-                  </th>
-                  <th key={data.id} scope="col">
-                    Sensor8
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr key={data.id}>
-                  {data.baris1.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr>
-                  {data.baris2.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr key={data.id}>
-                  {data.baris3.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr key={data.id}>
-                  {data.baris4.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr key={data.id}>
-                  {data.baris5.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr key={data.id}>
-                  {data.baris6.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr key={data.id}>
-                  {data.baris7.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-                <tr key={data.id}>
-                  {data.baris8.map((datasensor) => {
-                    return <td key={datasensor.id}>{datasensor}</td>;
-                  })}
-                </tr>
-              </tbody>
-            </table>
+            <div>
+              <button type="button" class="btn btn-primary mt-5">
+                {moment(data.subscribeDate).format("MMMM Do YYYY, h:mm:ss a")}
+              </button>
+
+              <table key={data.id} className="table mt-5">
+                <thead key={data.id}>
+                  <tr key={data.id}>
+                    <th key={data.id} scope="col">
+                      Sensor1
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor2
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor3
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor4
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor5
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor6
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor7
+                    </th>
+                    <th key={data.id} scope="col">
+                      Sensor8
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr key={data.id}>
+                    {data.baris1.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr>
+                    {data.baris2.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr key={data.id}>
+                    {data.baris3.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr key={data.id}>
+                    {data.baris4.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr key={data.id}>
+                    {data.baris5.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr key={data.id}>
+                    {data.baris6.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr key={data.id}>
+                    {data.baris7.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                  <tr key={data.id}>
+                    {data.baris8.map((datasensor) => {
+                      return <td key={datasensor.id}>{datasensor}</td>;
+                    })}
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           );
         })}
       </main>
